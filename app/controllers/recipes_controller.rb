36d2de707b73e3f492c7bd6973dad39e19782f61
John Everett <john.everett@shaw.ca>
2017-04-15 14:06:47 +0000

    Adding Flash message partial

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -16,7 +16,7 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new(recipe_params)
     @recipe.chef = Chef.first
     if @recipe.save
-      flash[:success] = "Recipe was created successfully"
+      flash[:success] = "Recipe was created successfully!"
       redirect_to recipe_path(@recipe)
     else
       render "new"
