ebf1d264729c76376aa45f3501b6bcfdaa77f4f3
John Everett <john.everett@shaw.ca>
2017-05-18 18:50:59 +0000

    add chehf destroy and with tests

--- test/models/chef_test.rb
+++ test/models/chef_test.rb
@@ -69,6 +69,15 @@ class ChefTest < ActiveSupport::TestCase
     @chef.password = @chef.password_confirmation = "x" * 4
     assert_not @chef.valid?
   end
+  
+  test "associated recipes should be destroyed" do
+    @chef.save 
+    @chef.recipes.create!(name: "testing destory", description: "testing destroy")
+    assert_difference 'Recipe.count',-1 do
+      @chef.destroy
+    end
+    
+  end
 
   
 end
